3ae4e8c6fe342204e2b2880742bab542f3daf7ea
Dmitriy <krivosheind28@gmail.com>
2021-11-29 23:29:50 +0300

    add lab 7, 8, 9, 10

--- main.c
+++ main.c
@@ -1,12 +1,8 @@
-#include "function/tools.h"
-#include "labs/lab_6/3.h"
+
+#include "labs/lab_10/6.h"
 
 int main() {
-    int y[] = {2, 1, 2, -1, 3, 5, 12, 4, 98, 12, 423, 0, 123, 87, 9, 5, 34, 8, 1, 55};
-    int size_x;
-    int x[] = {1, 3, 4, 5, 1};
-    task_3();
-//    int *x = input_array(&size_x);
-//    printf("%d", task_4(x, 5));
+
+    task();
     return 0;
 }
